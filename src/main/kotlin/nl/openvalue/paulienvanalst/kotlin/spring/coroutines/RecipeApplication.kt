7437d52498cf9b98085ed0f1a6deba6cc7d0b26c
Paulien van Alst <coding@paulienvanalst.com>
2021-05-30 17:33:36 +0200

    First setup database configuration

--- src/main/kotlin/nl/openvalue/paulienvanalst/kotlin/spring/coroutines/RecipeApplication.kt
+++ src/main/kotlin/nl/openvalue/paulienvanalst/kotlin/spring/coroutines/RecipeApplication.kt
@@ -2,8 +2,10 @@ package nl.openvalue.paulienvanalst.kotlin.spring.coroutines
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 
 @SpringBootApplication
+@EnableR2dbcRepositories
 open class RecipeApplication
 
 fun main(args: Array<String>) {
