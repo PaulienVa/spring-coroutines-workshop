77821204c480d05e0057d38e1609c5bf2bdbc78a
Paulien van Alst <coding@paulienvanalst.com>
2021-05-30 17:22:05 +0200

    Add code for endpoint implementation

---
+++ src/main/kotlin/nl/openvalue/paulienvanalst/kotlin/spring/coroutines/Recipes.kt
@@ -0,0 +1,4 @@
+package nl.openvalue.paulienvanalst.kotlin.spring.coroutines
+
+
+data class Recipe(val name: String, val duration: Int, val ingredients: List<String>, val preparation: String)
---
+++ src/main/kotlin/nl/openvalue/paulienvanalst/kotlin/spring/coroutines/RoutinConfiguration.kt
@@ -0,0 +1,16 @@
+package nl.openvalue.paulienvanalst.kotlin.spring.coroutines
+
+//import org.springframework.context.annotation.Bean
+//import org.springframework.context.annotation.Configuration
+//import org.springframework.web.reactive.function.server.RouterFunction
+//import org.springframework.web.reactive.function.server.ServerResponse
+//import org.springframework.web.reactive.function.server.coRouter
+
+//@Configuration
+open class RoutingConfiguration {
+
+//    @Bean
+//    open fun mainRouter(recipeHandler: RecipeHandler): RouterFunction<ServerResponse> =  coRouter {
+//    }
+
+}
